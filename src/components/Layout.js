31e3f6cab6c9ae63b9d1d5d6f313014272cc2e4d
Dominic <dominic.tobias@gmail.com>
2020-01-30 18:45:30 +0800

    Remove couple more unecessary lines to repro

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,8 +4,6 @@ import { MDXProvider } from "@mdx-js/react";
 import CodeBlock from "./CodeBlock";
 
 const components = {
-  h1: props => <h1 style={{ color: "tomato" }} {...props} />,
-  pre: props => <div {...props} />,
   code: CodeBlock
 };
 
